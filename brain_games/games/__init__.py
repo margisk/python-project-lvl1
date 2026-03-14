c8b6e211493a25413e7efee99fd94035c765d2f7
Nikita Kuzin <nickoskuzin@gmail.com>
2026-03-14 17:35:23 +0100

    implement the progression game

--- brain_games/games/__init__.py
+++ brain_games/games/__init__.py
@@ -1,3 +1,3 @@
-from brain_games.games import calc, even, gcd
+from brain_games.games import calc, even, gcd, progression
 
-__all__ = ["even", "calc", "gcd"]
+__all__ = ["even", "calc", "gcd", "progression"]
---
+++ brain_games/games/progression.py
@@ -0,0 +1,22 @@
+from random import randint
+
+INSTRUCTION = 'What number is missing in the progression?'
+
+
+def generate_qa_pair() -> tuple:
+	progression = generate_arithmetic_progression()
+	position_to_hide = randint(1, len(progression) - 1)
+	answer = progression[position_to_hide]
+	progression[position_to_hide] = '..'
+	return (progression, str(answer))
+
+
+def generate_arithmetic_progression() -> list:
+	start = randint(1, 100)
+	length = randint(5, 10)
+	step = randint(1, 10)
+	items = [start]
+	for i in range(1, length):
+		curr_item = start + i * step
+		items.append(curr_item)
+	return items
